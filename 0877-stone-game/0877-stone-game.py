9f5c0d4779990b711d83e6d55b1ee2b736637450
Abhash Kumar Singh <161006320+lostParsley@users.noreply.github.com>
2026-08-02 20:32:53 +0530

    Time: 259 ms (31.38%), Space: 54 MB (27.62%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,7 +1,7 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         n = len(piles)
-        dp = [[-1] * n for _ in range(n)]
+        dp = [[0] * (n+1) for _ in range(n+1)]
         def rec(i , j):
             if i == j:
                 return piles[i]
@@ -11,5 +11,10 @@ class Solution:
                 piles[i] - rec(i+1 , j) , piles[j] - rec(i , j-1)
             )
             return dp[i][j]
-        return rec(0 ,n-1) > 0 
+        for i in range(n-1 , -1 , - 1):
+            for j in range(n):
+                dp[i][j] = max(piles[i] - dp[i+1][j] , piles[j] - dp[i][j+1])
+        
+        # return rec(0 ,n-1) > 0 
+        return dp[0][n-1] > 0
 
